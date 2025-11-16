897d267b38bcec07a2cd80ce10c07c3fa6d62f32
Rodrigo Hernandez <rodrigo.hernandez.zelada@gmail.com>
2025-11-15 21:18:57 -0300

    Agregar CI básico con Github Actions

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,6 @@
 const express = require("express");
 const app = express();
 
-<<<<<<< HEAD
-const PORT = process.env.PORT || 3000;
-
-app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send("MVP -backend funcionando");
-});
-
-=======
 // Middleware para parsear JSON
 app.use(express.json());
 
@@ -21,7 +11,6 @@ app.get("/", (req, res) => {
 
 // Iniciar servidor
 const PORT = 3000;
->>>>>>> a9a77ee (Recrear index.js para backend Express)
 app.listen(PORT, () => {
   console.log(`Servidor escuchando en http://localhost:${PORT}`);
 });
